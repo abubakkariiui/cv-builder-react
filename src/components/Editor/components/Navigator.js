a4c744fd26adb7071496b30975c68ef67ce722eb
abubakkariiui <abubakkarmit@gmail.com>
2023-02-10 11:42:40 +0500

    update contact info form

--- src/components/Editor/components/Navigator.js
+++ src/components/Editor/components/Navigator.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-mixed-operators */
-import {
-  RiArrowLeftSLine,
-  RiArrowRightSLine,
-} from 'react-icons/ri';
-import Button from './Button';
+import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
+import Button from "./Button";
 
 function Navigator(props) {
   const {
@@ -20,33 +17,37 @@ function Navigator(props) {
 
   return (
     <div className="Navigator">
-      {firstButton || items[previousIndex] && (
-        <Button
-          icon={<RiArrowLeftSLine />}
-          label={items[previousIndex]?.name}
-          handleClick={() => setIndex(previousIndex)}
-          alt
-        />
-      ) || spacer}
+      {firstButton ||
+        (items[previousIndex] && (
+          <Button
+            icon={<RiArrowLeftSLine />}
+            label={items[previousIndex]?.name}
+            handleClick={() => setIndex(previousIndex)}
+            alt
+          />
+        )) ||
+        spacer}
       {useDots && (
         <div className="Dots">
           {items.map(({ key }, i) => (
             <div
               key={`${key}-dot`}
-              className={`Dot ${index === i ? 'active' : ''}`}
+              className={`Dot ${index === i ? "active" : ""}`}
             />
           ))}
         </div>
       )}
-      {lastButton || items[nextIndex] && (
-        <Button
-          icon={<RiArrowRightSLine />}
-          iconPosition="right"
-          label={items[nextIndex]?.name}
-          handleClick={() => setIndex(nextIndex)}
-          alt
-        />
-      ) || spacer}
+      {lastButton ||
+        (items[nextIndex] && (
+          <Button
+            icon={<RiArrowRightSLine />}
+            iconPosition="right"
+            label={items[nextIndex]?.name}
+            handleClick={() => setIndex(nextIndex)}
+            alt
+          />
+        )) ||
+        spacer}
     </div>
   );
 }
